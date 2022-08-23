b22d5eb7113c977a280318e64bc1202cdaa353be
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-23 21:59:18 +0300

    commit tree 0f1bb5306db90fb3c473286753a163092eccd73a

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears(id INTEGER PRIMARY KEY, 
+name TEXT,
+age INT,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 25, 'M', 'black', 'Sanguine', true),
+('Rowdy', 20, 'M', 'white', 'Phlegmatic', false),
+('Tabitha', 39, 'F', 'white', 'Melacholic', true),
+('Sergeant Brown', 30, 'M', 'Brown', 'Phlegmatic', true),
+('Melissa', 40, 'F', 'white', 'Choleric', true),
+('Grinch', 18, 'M', 'chocolate', 'Phlegmatic', false),
+('Wendy', 28, 'F', 'black', 'Sanguine', true),
+(NULL, 24, 'F', 'white', 'Choleric', false);
